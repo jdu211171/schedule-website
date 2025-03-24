3c279a68517692d8abae32e6ff5f1526d63588ad
jdu211171 <211171m@jdu.uz>
2025-03-24 13:32:52 +0500

    Add Vercel Analytics integration and update dependencies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-// import { ThemeToggle } from "@/components/theme-toggle";
+import {Analytics} from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,8 +28,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {/* <ThemeToggle /> */}
         {children}
+        <Analytics />
       </body>
     </html>
   );
